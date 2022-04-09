20b688b2c3cf18c413be46ffa9bb9ccd447c52e5
Josecontis <peppeco98@gmail.com>
2022-04-09 19:30:16 +0200

    Add TopBar component in homePage

--- il_belvedere/src/App.js
+++ il_belvedere/src/App.js
@@ -1,22 +1,26 @@
 import "./App.css";
+import TopBar from "./Components/TopBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TopBar />
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
   );
 }
 
---
+++ il_belvedere/src/Components/TopBar.js
@@ -0,0 +1,33 @@
+import "./TopBar.css";
+import { HiOutlineMail } from "react-icons/hi";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { HiOutlinePhotograph } from "react-icons/hi";
+import { FiPhoneCall } from "react-icons/fi";
+import logoHome from "../Assets/Image/logoHomeSmall.png";
+
+function TopBar() {
+  return (
+    <div className="top-bar">
+      <img src={logoHome} />
+      <div className="info">
+        <HiOutlineMail />
+        <a href="mailto:ilBelvedere@gmail.com">ilBelvedere@gmail.com</a>
+      </div>
+      <div className="info">
+        <HiOutlineLocationMarker />
+        <a href="mailto:ilBelvedere@gmail.com">Dove Siamo</a>
+      </div>
+      <div className="info">
+        <FiPhoneCall />
+        <a href="callto:3281349208">Cell.: (+39) 328 1349208</a>
+      </div>
+      <div className="info">
+        <HiOutlinePhotograph />
+        <a href="callto:3281349208">Itinerari</a>
+      </div>
+      <button>PRENOTA</button>
+    </div>
+  );
+}
+
+export default TopBar;
