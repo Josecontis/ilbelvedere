c33f0d5292ad2d8449344a093fdeafeb672623f1
Josecontis <peppeco98@gmail.com>
2022-04-10 12:32:47 +0200

    Add scroll to top button

--- il_belvedere/src/App.js
+++ il_belvedere/src/App.js
@@ -3,11 +3,33 @@ import "./App.css";
 import TopBar from "./Components/TopBar";
 
 function App() {
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      document.getElementById("myBtn").style.display = "block";
+    } else {
+      document.getElementById("myBtn").style.display = "none";
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
   return (
     <>
       <TopBar />
       <div className="App">
-        <header className="App-header"></header>
+        <div className="App-content"></div>
         <a
           class="watsapp-button"
           data-action="open"
@@ -18,6 +40,14 @@ function App() {
         >
           <AiOutlineWhatsApp />
         </a>
+        <button
+          className="button-to-top"
+          onClick={topFunction}
+          id="myBtn"
+          title="Go to top"
+        >
+          Top
+        </button>
       </div>
     </>
   );
