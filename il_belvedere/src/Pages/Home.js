5f74e6eee1d180b7482aabdc221d69d8ca1e5ef2
Josecontis <peppeco98@gmail.com>
2022-04-15 20:41:48 +0200

    Fix translation animation and add custom text

--- il_belvedere/src/Pages/Home.js
+++ il_belvedere/src/Pages/Home.js
@@ -3,27 +3,31 @@ import bedImg from "../Assets/Image/336261885crop.jpg";
 import bathImg from "../Assets/Image/342667958crop.jpg";
 
 const Home = () => {
-  var myIndex = 0;
-  carousel();
+  let slideIndex = 0;
+  showSlides();
 
-  function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
     }
-    myIndex++;
-    if (x && myIndex && myIndex > x.length) {
-      myIndex = 1;
+    slideIndex++;
+    if (slideIndex && slideIndex > slides.length) {
+      slideIndex = 1;
     }
-    x[myIndex - 1] && (x[myIndex - 1].style.display = "block");
-
-    setTimeout(carousel, 6000);
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1] && (slides[slideIndex - 1].style.display = "block");
+    dots[slideIndex - 1] && (dots[slideIndex - 1].className += " active");
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
   }
 
   return (
     <>
-      <div className="w3-content w3-section">
+      {/* <div className="w3-content w3-section">
         <div className="mySlides">
           <img
             alt="cc1"
@@ -40,6 +44,21 @@ const Home = () => {
             style={{ width: "100%" }}
           />
         </div>
+      </div> */}
+      <div class="slideshow-container">
+        <div class="mySlides fade">
+          <img alt="cc" src={bedImg} style={{ width: "100%" }} />
+          <div class="text">Benvenuti</div>
+        </div>
+        <div class="mySlides fade">
+          <img alt="cc1" src={bathImg} style={{ width: "100%" }} />
+        </div>
+      </div>
+      <br />
+      <div style={{ textAlign: "center" }}>
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
       </div>
     </>
   );
